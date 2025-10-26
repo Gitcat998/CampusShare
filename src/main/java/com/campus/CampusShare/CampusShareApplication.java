68095188c258085c3421444e1175f3200a6c3d89
gitcat998 <2633796835@qq.com>
2025-10-26 16:42:57 +0800

    第2次修改

--- src/main/java/com/campus/CampusShare/CampusShareApplication.java
+++ src/main/java/com/campus/CampusShare/CampusShareApplication.java
@@ -3,7 +3,7 @@ package com.campus.CampusShare;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 /**
  * Spring Boot 应用启动类
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @MapperScan("com.campus.CampusShare.mapper")
-@ComponentScan(basePackages = "com.campus.CampusShare")
+
 public class CampusShareApplication {
 
     public static void main(String[] args) {
